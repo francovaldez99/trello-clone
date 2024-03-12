ce16cf55dc9eebb7e6a13d9ff69a29fa0d496161
Franco Valdez <franco.martin.valdez@mi.unc.edu.ar>
2024-03-11 23:00:45 -0300

    cambios en conexion a base de datos -render.com

--- server/src/config/dbConfig.js
+++ server/src/config/dbConfig.js
@@ -5,6 +5,7 @@ const sequelize = new Sequelize(
   DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
     dialect:"postgres",
+    logging: false,
     dialectOptions: {
       ssl: {
         require: true, 
