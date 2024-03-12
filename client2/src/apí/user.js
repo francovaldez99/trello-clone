dcd7d298c325fe7bd23c9288eb8525bd69ed0425
Franco Valdez <franco.martin.valdez@mi.unc.edu.ar>
2024-03-12 00:46:41 -0300

    cambios en conexion a base de datos -render.com port api

--- client2/src/apí/user.js
+++ client2/src/apí/user.js
@@ -1,6 +1,6 @@
 import axios from "./axios"
 
 
-export const register =(user)=>axios.post("api/user/register",user)
-export const login = (user)=>axios.post("api/user/login",user)
-export const verifyToken=(token)=>axios.get("api/user/verify-token",{token})
+export const register =(user)=>axios.post("/api/user/register",user)
+export const login = (user)=>axios.post("/api/user/login",user)
+export const verifyToken=(token)=>axios.get("/api/user/verify-token",{token})
