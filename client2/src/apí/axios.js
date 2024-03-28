cb71c8dc5cc6c2e9605c75bd972bda2ef423b346
Franco Valdez <franco.martin.valdez@mi.unc.edu.ar>
2024-03-28 17:46:36 -0300

    resolviendo cookie 042

--- client2/src/apí/axios.js
+++ client2/src/apí/axios.js
@@ -3,7 +3,11 @@ import { URL_SERVER } from "../config"
 
 const axiosInstance=axios.create({
     baseURL:URL_SERVER,
-   withCredentials: true
+   withCredentials: true,
+
+    headers: {
+        'Access-Control-Allow-Origin': '*', 
+        'Content-Type': 'application/json'}
 })
 
 export default axiosInstance
