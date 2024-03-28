cfb099c563cf46277abfb0965ae577752b3f5ab8
Franco Valdez <franco.martin.valdez@mi.unc.edu.ar>
2024-03-28 17:45:35 -0300

    resolviendo cookie 041

--- client2/src/apí/axios.js
+++ client2/src/apí/axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { URL_SERVER } from "../config"
 
 const axiosInstance=axios.create({
-    baseURL:"https://trello-clone-server-cnaq.onrender.com",
+    baseURL:URL_SERVER,
    withCredentials: true
 })
 
