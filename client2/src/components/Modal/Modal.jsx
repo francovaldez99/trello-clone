a31f953398c6ab88e92c1201ee132f3a46cc7625
Franco Valdez <franco.martin.valdez@mi.unc.edu.ar>
2024-04-16 20:53:51 -0300

    correcciones en padding modal

--- client2/src/components/Modal/Modal.jsx
+++ client2/src/components/Modal/Modal.jsx
@@ -2,8 +2,8 @@
 function Modal({ isOpen, setIsOpen, children }) {
   if (isOpen) {
     return (
-      <div className="fixed bg-[rgba(0,0,0,0.6)] top-0 bottom-0 left-0 right-0 z-[1]">
-        <div className="relative flex items-center justify-center content-center flex-row h-screen">
+      <div className="fixed bg-[rgba(0,0,0,0.6)] top-0 bottom-0 left-0 right-0 z-[1] ">
+        <div className="relative flex items-center justify-center content-center flex-row h-screen py-8 ">
           {children}
         </div>
       </div>
